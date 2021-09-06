1bcc62711fe2ec1c0dee112763a0c17f8fde3561
j99120701 <helen_lan@gss.com.tw>
2021-09-06 15:40:26 +0800

    css and default selection fixed

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 //Loader
-const loader = document.getElementById("loader");
-const myDiv = document.getElementById("myDiv");
+const g_loader = document.getElementById("loader");
+const contentDiv = document.getElementById("contentDiv");
 // get html document
-const selectAdd = document.getElementById("selectAdd");
+const g_selectAdd = document.getElementById("selectAdd");
 const selectedText = document.getElementById("mainText");
 //熱門地區button
 //When using getElementsByClassName, you will get array of elements instead of single array unlike getElementById.
-const hotBtn = document.getElementsByClassName("btn")[0];
-const hotBtn1 = document.getElementsByClassName("b1")[0];
-const hotBtn2 = document.getElementsByClassName("b2")[0];
-const hotBtn3 = document.getElementsByClassName("b3")[0];
-const hotBtn4 = document.getElementsByClassName("b4")[0];
+const hotBtn = document.getElementById("btn");
+const g_$hotBtn1 = document.getElementById("hotBtn1");
+const g_$hotBtn2 = document.getElementById("hotBtn2");
+const g_$hotBtn3 = document.getElementById("hotBtn3");
+const g_$hotBtn4 = document.getElementById("hotBtn4");
 //各地區Box
-const addrBox = document.getElementById("addrBox");
+const g_addrBox = document.getElementById("addrBox");
 //現在分頁
-let pagination = document.getElementById("pagination");
+let g_pagination = document.getElementById("pagination");
 // 限制每頁最多6筆資料
-const perPage = 6;
+const g_perPage = 6;
 
 let addressArr = []; //景點資料陣列
 let addrArray = []; //整理過的高雄各地區名(不重複地區名)
 //過濾所有陣列中的地區(addressArr)，將需要的地區過濾出來，回傳api資料的index
 let filterAddr = [];
 //下拉式選單，選擇完成的地區
-let selectedAddrText = "";
+let g_selectedAddrText = "";
 
 //send request
 let request = "";
@@ -38,99 +38,108 @@ if (window.XMLHttpRequest) {
 }
 
 request.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    let data = JSON.parse(this.responseText);
-    let info = data.data.XML_Head.Infos.Info; // 只需要Info的資料
-    //整理出不重複的地名
-    for (let i = 0; i < info.length; i++) {
-      //暫時印出所有地名，想過濾方法
-      //現在將 Picture1 ,Name, Add, Opentime, Tel 加入陣列
-      addressArr.push([
-        info[i].Picture1,
-        info[i].Name,
-        info[i].Add,
-        info[i].Opentime,
-        info[i].Tel,
-      ]);
-      //取出景點行政區名（有重複的）
-      let addrName = info[i].Add.slice(6, 9);
-      if (!(addrArray.indexOf(addrName) > -1)) {
-        addrArray.push(addrName);
+  try {
+    if (this.readyState == 4 && this.status == 200) {
+      let data = JSON.parse(this.responseText);
+      let info = data.data.XML_Head.Infos.Info; // 只需要Info的資料
+      //整理出不重複的地名
+      for (let i = 0; i < info.length; i++) {
+        //暫時印出所有地名，想過濾方法
+        //現在將 Picture1 ,Name, Add, Opentime, Tel 加入陣列
+        addressArr.push([
+          info[i].Picture1,
+          info[i].Name,
+          info[i].Add,
+          info[i].Opentime,
+          info[i].Tel,
+        ]);
+        //取出景點行政區名（有重複的）
+        let addrName = info[i].Add.slice(6, 9);
+        if (!(addrArray.indexOf(addrName) > -1)) {
+          addrArray.push(addrName);
+        }
       }
+      // 將不重複的地名陣列，利用forEach加入option
+      // addrArray.forEach((addr, index) => {
+      //   let option = document.createElement("option");
+      //   option.value = index;
+      //   option.innerHTML = addr;
+      //   g_selectAdd.appendChild(option);
+      // });
+      // 將不重複的地名陣列，利用forEach加入option ==> IE相容性
+      for (let index = 0; index < addrArray.length; index++) {
+        let option = document.createElement("option");
+        option.value = index;
+        option.innerHTML = addrArray[index];
+        g_selectAdd.appendChild(option);
+      }
+      g_loader.style.display = "none";
+      contentDiv.style.display = "block";
+      //預設行政區
+      g_selectAdd.value = 0;
+      selectedText.innerText = addrArray[0];
+      changeSelectedOption(0);
     }
-    // 將不重複的地名陣列，利用forEach加入option
-    // addrArray.forEach((addr, index) => {
-    //   let option = document.createElement("option");
-    //   option.value = index;
-    //   option.innerHTML = addr;
-    //   selectAdd.appendChild(option);
-    // });
-    // 將不重複的地名陣列，利用forEach加入option ==> IE相容性
-    for (let index = 0; index < addrArray.length; index++) {
-      let option = document.createElement("option");
-      option.value = index;
-      option.innerHTML = addrArray[index];
-      selectAdd.appendChild(option);
-    }
-    loader.style.display = "none";
-    myDiv.style.display = "block";
+  } catch (error) {
+    alert(error);
   }
 };
 //第三個參數true是非同步
 request.open("GET", baseURL, true);
 request.send(null);
 
-function addrChange(selectedAddr) {
+function changeSelectedOption(selectedAddr) {
   //selectedText非空（已經選擇過地區）
   let selected = selectedAddr.innerText;
   if (selectedText.innerText != " ") {
     if (
-      selected != hotBtn1.innerText ||
-      selected != hotBtn2.innerText ||
-      selected != hotBtn3.innerText ||
-      selected != hotBtn4.innerText
+      selected != g_$hotBtn1.innerText ||
+      selected != g_$hotBtn2.innerText ||
+      selected != g_$hotBtn3.innerText ||
+      selected != g_$hotBtn4.innerText
     ) {
       //其他的按鈕要是disabled = false
       //先把所有熱門地區Bt0n還原
-      hotBtn1.style.cssText = "";
-      hotBtn1.disabled = false;
-      hotBtn2.style.cssText = "";
-      hotBtn2.disabled = false;
-      hotBtn3.style.cssText = "";
-      hotBtn3.disabled = false;
-      hotBtn4.style.cssText = "";
-      hotBtn4.disabled = false;
+      g_$hotBtn1.style.cssText = "";
+      g_$hotBtn1.disabled = false;
+      g_$hotBtn2.style.cssText = "";
+      g_$hotBtn2.disabled = false;
+      g_$hotBtn3.style.cssText = "";
+      g_$hotBtn3.disabled = false;
+      g_$hotBtn4.style.cssText = "";
+      g_$hotBtn4.disabled = false;
     }
     filterAddr = [];
     //一轉換地區，就清空原本div內的Box
-    addrBox.innerHTML = "";
+    g_addrBox.innerHTML = "";
     if (typeof selectedAddr != "number") {
-      selectedAddrText = selectedAddr.options[selectedAddr.selectedIndex].text;
+      g_selectedAddrText =
+        selectedAddr.options[selectedAddr.selectedIndex].text;
     } else {
       //修改目前選到地區的文字
-      selectedAddrText = addrArray[selectedAddr];
+      g_selectedAddrText = addrArray[selectedAddr];
       // //也要改變下拉式選單的選項
-      selectAdd.selectedIndex = selectedAddr + 1;
+      g_selectAdd.selectedIndex = selectedAddr + 1;
     }
 
     //改變目前選擇地區 p -> div | getElementsByClassName -> getElementById
-    selectedText.innerHTML = selectedAddrText;
+    selectedText.innerHTML = g_selectedAddrText;
     //self 用不到
     addressArr.filter(function (element, index, self) {
       //element[2]放的是地名
       //includes IE not support... 改用indexOf
-      if (element[2].indexOf(selectedAddrText) != -1) {
+      if (element[2].indexOf(g_selectedAddrText) != -1) {
         filterAddr.push(element);
       }
     });
     //新增已過濾完成的地區名Box (前6筆資料)
     //如果原本地區的景點數量不到6筆
-    if (filterAddr.length < perPage) {
+    if (filterAddr.length < g_perPage) {
       for (let i = 0; i < filterAddr.length; i++) {
         createAddressBox(i);
       }
     } else {
-      for (let i = 0; i < perPage; i++) {
+      for (let i = 0; i < g_perPage; i++) {
         createAddressBox(i);
       }
     }
@@ -146,25 +155,26 @@ function createAddressBox(index) {
   let imgUrl = filterAddr[index][0];
   let createImg = document.createElement("img");
   createImg.src = imgUrl;
-  //每一次新增的Box的css
-  let boxContent =
-    "border: 1px solid rgba(0, 0, 0, 0.1);padding: 20px 10px;display: flex;background: rgba(255, 255, 255, 0.432);justify-content: center;align-items: center;flex-direction: column;border-radius: 10px";
-  createBox.style.cssText = boxContent;
   boxText.innerHTML =
+    "<div class='dataBox' style='background-image:url(" +
+    imgUrl +
+    ");background-size: 100% 200px; background-repeat: no-repeat;'><div class='dataTitle'>" +
     filterAddr[index][1] +
+    "</div><div class='dataText'>" +
     "<br><br><i class='fas fa-clock'></i>&nbsp;&nbsp;" +
     filterAddr[index][3] +
     "<br><br><i class='fas fa-map-marker-alt'></i>&nbsp;&nbsp;" +
     filterAddr[index][2] +
     "<br><br><i class='fas fa-mobile-alt'></i>&nbsp;&nbsp;" +
-    filterAddr[index][4];
+    filterAddr[index][4] +
+    "</div></div>";
   // 新增div
-  createBox.appendChild(createImg);
+  // createBox.appendChild(createImg);
   createBox.appendChild(boxText);
-  addrBox.appendChild(createBox);
+  g_addrBox.appendChild(createBox);
 }
 //分頁-監聽點擊事件
-pagination.addEventListener("click", function (e) {
+g_pagination.addEventListener("click", function (e) {
   e.preventDefault();
   if (e.target.tagName === "A") {
     //點擊到分頁號碼，篩選出該頁面的資料後，顯示
@@ -182,7 +192,7 @@ hotBtn.addEventListener("click", function (e) {
       for (let i = 0; i < addrArray.length; i++) {
         if (addrArray[i] === value) {
           //找到被點擊按鈕在addrArray的index
-          addrChange(i);
+          changeSelectedOption(i);
         }
       }
       e.target.disabled = true;
@@ -194,10 +204,10 @@ hotBtn.addEventListener("click", function (e) {
 // 篩選分頁資料
 function getPageData(choosePage, allData) {
   //先清空頁面資料
-  addrBox.innerHTML = "";
-  let offset = (choosePage - 1) * perPage;
+  g_addrBox.innerHTML = "";
+  let offset = (choosePage - 1) * g_perPage;
   //分好頁的資料陣列
-  let thisPageData = allData.slice(offset, offset + perPage);
+  let thisPageData = allData.slice(offset, offset + g_perPage);
   for (let i = 0; i < thisPageData.length; i++) {
     //createElement 如果放在最外面，就會只有一個div可以用，所以要放在裡面
     let createBox = document.createElement("div"); //包含照片跟資訊
@@ -207,7 +217,7 @@ function getPageData(choosePage, allData) {
     createImg.src = imgUrl;
     //每一次新增的Box的css
     let boxContent =
-      "border: 1px solid rgba(0, 0, 0, 0.1);padding: 20px 10px;display: flex;background: rgba(255, 255, 255, 0.432);justify-content: center;align-items: center;flex-direction: column;border-radius: 10px";
+      "border: 3px solid rgba(0, 0, 0, 0.5);padding: 20px 10px;display: flex;background: rgba(255, 255, 255, 0.432);justify-content: center;align-items: center;flex-direction: column;border-radius: 10px";
     createBox.style.cssText = boxContent;
     boxText.innerHTML =
       thisPageData[i][1] +
@@ -220,14 +230,14 @@ function getPageData(choosePage, allData) {
     // 新增div
     createBox.appendChild(createImg);
     createBox.appendChild(boxText);
-    addrBox.appendChild(createBox);
+    g_addrBox.appendChild(createBox);
   }
 }
 //計算總共頁數
 function getTotalPage(addressArr) {
   //先清空原本頁碼
-  pagination.innerHTML = "";
-  let totalPage = Math.ceil(addressArr.length / perPage) || 1;
+  g_pagination.innerHTML = "";
+  let totalPage = Math.ceil(addressArr.length / g_perPage) || 1;
   let pageItemStyle =
     "margin: 30px;text-decoration: none;text-align: center; justify-content: center;align-items: center; width:300px;";
   let pageItemContent = "";
@@ -241,6 +251,6 @@ function getTotalPage(addressArr) {
       (i + 1) +
       "</a></li>";
   }
-  pagination.style.cssText = pageItemStyle;
-  pagination.innerHTML = pageItemContent;
+  g_pagination.style.cssText = pageItemStyle;
+  g_pagination.innerHTML = pageItemContent;
 }
